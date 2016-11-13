4e7e70cbad4a9d5783497d06fe37db03556ec17b
Kenneth Siu <kennethsiu11@gmail.com>
2016-11-13 16:52:03 +0000

    File renaming

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: "./src/visualSpoiler.tsx",
     output: {
-        filename: "bundle.js",
+        filename: "visualSpoilerBundle.js",
         path: __dirname + "/dist"
     },
 
