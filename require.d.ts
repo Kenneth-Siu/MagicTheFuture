73c06c51920ceaf74015607265f4e8292fd6d450
Kenneth Siu <kennethsiu11@gmail.com>
2016-11-21 23:44:59 +0000

    Remove require.d.ts because @types/node is a dependency

--- require.d.ts
+++
@@ -1,6 +0,0 @@
-// require.d.ts
-declare var require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
