4f5b4b11ab53093b2b15a624b75d57bbf9e1e414
Kenneth Siu <Kenneth.Siu@softwire.com>
2016-12-12 20:32:50 +0000

    Be able to see what the AI thinks of the pack

--- src/draftSim/CardPicker.ts
+++ src/draftSim/CardPicker.ts
@@ -4,16 +4,7 @@ import Card from "../common/Card";
 import { Color, SingleColor } from "../common/Card";
 import ColorAnalyser from "./ColorAnalyser";
 import SynergyAnalyser from "./SynergyAnalyser";
-
-class CardRating {
-    card: Card;
-    rating: number;
-
-    constructor(card: Card, rating: number) {
-        this.card = card;
-        this.rating = rating;
-    }
-}
+import CardRating from "./CardRating";
 
 export default class CardPicker {
     picks: Card[];
@@ -22,22 +13,22 @@ export default class CardPicker {
     decidePick(pack: Pack, picks: Card[]): Card {
         if (!pack || pack.cards.length === 0) throw "Sorry Dave, I can't let you pick from a nonexistent or empty pack.";
         this.picks = picks;
-        this.evaluatePack(pack);
+        this.evaluatePack(picks, pack);
         return this.makePick();
     }
 
-    private evaluatePack(pack: Pack): void {
-        this.packRatings = pack.cards.map(card => this.evaluateCard(card));
-    }
-
-    private evaluateCard(card: Card): CardRating {
-        const colorAnalyser = new ColorAnalyser(this.picks, card);
-        const synergyAnalyser = new SynergyAnalyser(this.picks, card);
-        const pickRatings = this.picks.map(pick => {
+    evaluateCard(picks: Card[], card: Card): CardRating {
+        const colorAnalyser = new ColorAnalyser(picks, card);
+        const synergyAnalyser = new SynergyAnalyser(picks, card);
+        const pickRatings = picks.map(pick => {
             return synergyAnalyser.getExistingPowerOfCard(pick) * colorAnalyser.getModifier(pick.color)
         }).reduce((a, b) => a + b, 0);
         const cardRating = synergyAnalyser.getPotentialPowerOfCard(card) * colorAnalyser.getModifier(card.color);
         return new CardRating(card, pickRatings + cardRating);
+    }
+
+    private evaluatePack(picks: Card[], pack: Pack): void {
+        this.packRatings = pack.cards.map(card => this.evaluateCard(picks, card));
     }
 
     private makePick(): Card {
