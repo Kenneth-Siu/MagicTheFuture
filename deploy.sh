99fd8fdc5b296c3bb52933395ead0b4ec610d8ab
Kenneth Siu <kennethsiu11@gmail.com>
2016-12-13 23:25:09 +0000

    Getting github pages up and running

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+cp -R dist/ ../magictheconvergence
+cp -R views/ ../magictheconvergence
+cp node_modules/react/dist/react.min.js ../magictheconvergence/node_modules/react/dist/react.min.js
+cp node_modules/react-dom/dist/react-dom.min.js ../magictheconvergence/node_modules/react-dom/dist/react-dom.min.js
+cp favicon.ico ../magictheconvergence/favicon.ico
+cp index.html ../magictheconvergence/index.html
+
+cd ../magictheconvergence
+git checkout master
+echo "Are we on master?"
+read
+git add .
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+git commit -m "Deploy on $DATE"
+git push
+
+echo "Done"
+read
