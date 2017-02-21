40fd45241e1e8a3ed8cdca484b8968e3f0fead26
Kenneth Siu <kennethsiu11@gmail.com>
2017-02-21 23:26:27 +0000

    Clear dist folder when starting a watch

--- watch.sh
+++ watch.sh
@@ -1,3 +1,4 @@
+rm ../magictheconvergence/dist/*
 webpack --progress --colors --watch &
 less-watch-compiler src/visualSpoiler/less dist visualSpoiler.less &
 less-watch-compiler src/draftSim/less dist draftSim.less &
